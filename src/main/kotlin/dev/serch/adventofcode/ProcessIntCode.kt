0875c57c9347d99d88adde3cbdc8e6cb35af5d5f
Sergio Arroyo <sergio.arroyo.cuevas@gmail.com>
2019-12-04 10:49:37 +0100

    Solve day 2.2 problem

--- src/main/kotlin/dev/serch/adventofcode/ProcessIntCode.kt
+++ src/main/kotlin/dev/serch/adventofcode/ProcessIntCode.kt
@@ -1,22 +1,24 @@
 package dev.serch.adventofcode
 
+import dev.serch.adventofcode.ProcessIntCode.Companion.replace
+
 class ProcessIntCode {
 
     operator fun invoke(intCode: List<Int>): List<Int> {
         return process(intCode, 0)
     }
 
-    private tailrec fun process(intCode: List<Int>, nextOperation: Int): List<Int> {
-        val opCode = intCode[nextOperation]
+    private tailrec fun process(intCode: List<Int>, instructionPointer: Int): List<Int> {
+        val opCode = intCode[instructionPointer]
         val processed = when (opCode) {
             SUM_CODE ->
-                Triple(sum(intCode, nextOperation + 1), true, nextOperation + 4)
+                Triple(sum(intCode, instructionPointer + 1), true, instructionPointer + 4)
             MULTIPLY_CODE ->
-                Triple(multiply(intCode, nextOperation + 1), true, nextOperation + 4)
+                Triple(multiply(intCode, instructionPointer + 1), true, instructionPointer + 4)
             STOP_CODE ->
-                Triple(intCode, false, nextOperation)
+                Triple(intCode, false, instructionPointer)
             else ->
-                Triple(intCode, false, nextOperation)
+                Triple(intCode, false, instructionPointer)
         }
 
         return if (!processed.second || processed.third >= intCode.size) {
@@ -47,5 +49,20 @@ class ProcessIntCode {
         const val STOP_CODE = 99
 
         fun <E> Iterable<E>.replace(index: Int, elem: E) = mapIndexed { i, existing -> if (i == index) elem else existing }
+    }
+}
+
+class FindNounAndVerb(private val processIntCode: ProcessIntCode = ProcessIntCode()) {
+    operator fun invoke(opCode: List<Int>, expected: Int): Int? {
+        val allowedValues = 0..99
+        allowedValues.forEach { noun ->
+            allowedValues.forEach { verb ->
+                val input1202 = opCode.replace(1, noun).replace(2, verb)
+                if (processIntCode(input1202).first() == expected) {
+                    return noun * 100 + verb
+                }
+            }
+        }
+        return null
     }
 }
